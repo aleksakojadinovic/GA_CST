814591630941811c12e354b1d67f0b6e20fac1ad
Aleksa Kojadinovic <kojadinovic.aleksa98@gmail.com>
2023-06-18 15:03:58 +0200

    idk

--- algoritmi_sa_vezbi/constant_work_space.cpp
+++ algoritmi_sa_vezbi/constant_work_space.cpp
@@ -18,11 +18,14 @@ ConstantWorkSpace::ConstantWorkSpace(QWidget *qw,
         readFromFile(fileName);
     }
     preparePoints();
-    for (int i = 0; i < 20; i++)
-    {
-        QColor color(qrand() % 256, qrand() % 256, qrand() % 256);
-        colors.push_back(color);
-    }
+    colors.push_back(Qt::red);
+    colors.push_back(Qt::green);
+    colors.push_back(Qt::blue);
+    // for (int i = 0; i < 20; i++)
+    // {
+    //     QColor color(qrand() % 256, qrand() % 256, qrand() % 256);
+    //     colors.push_back(color);
+    // }
 }
 
 int ConstantWorkSpace::getLeftMostPointIndex(int fromIndex)
@@ -58,12 +61,13 @@ bool ConstantWorkSpace::isInteriorBelowLine(QLineF line)
     // This will be true if
 }
 
-void ConstantWorkSpace::find_biding_edge(int qi_index, bool * is_found, QLineF * edge, bool is_upper, int * edge_start_index) {
+void ConstantWorkSpace::find_biding_edge(int qi_index, bool *is_found, QLineF *edge, bool is_upper, int *edge_start_index)
+{
 
-    auto orientation = [](QPointF p, QPointF q, QPointF r) {
+    auto orientation = [](QPointF p, QPointF q, QPointF r)
+    {
         return (q.y() - p.y()) * (r.x() - q.x()) - (q.x() - p.x()) * (r.y() - q.y());
     };
-
 
     float best_intersection_y = is_upper ? -100000 : 100000;
     QPointF point = polygon[qi_index];
@@ -72,7 +76,8 @@ void ConstantWorkSpace::find_biding_edge(int qi_index, bool * is_found, QLineF *
 
     QLineF ray(point, QPointF(point.x(), is_upper ? 10000 : -10000));
 
-    for (int i = 0; i < polygon.size(); i++){
+    for (int i = 0; i < polygon.size(); i++)
+    {
         auto edge_p1 = polygon[i];
         auto edge_p2 = polygon[(i + 1) % polygon.size()];
 
@@ -84,18 +89,21 @@ void ConstantWorkSpace::find_biding_edge(int qi_index, bool * is_found, QLineF *
 
         auto intersection_result = ray.intersect(candidate_edge, &intersection);
 
-        if (intersection_result != QLineF::IntersectType::BoundedIntersection) {
+        if (intersection_result != QLineF::IntersectType::BoundedIntersection)
+        {
             continue;
         }
 
         // handle incident edge
 
-        if (qi_index == i || qi_index == (i + 1) % polygon.size()) {
+        if (qi_index == i || qi_index == (i + 1) % polygon.size())
+        {
             // handle incident edge
 
             // 1) edge needs to have an endpoint to the right of qi
             auto edge_max_right = std::max(edge_p1.x(), edge_p2.x());
-            if (edge_max_right <= point.x()) {
+            if (edge_max_right <= point.x())
+            {
                 continue;
             }
 
@@ -103,15 +111,18 @@ void ConstantWorkSpace::find_biding_edge(int qi_index, bool * is_found, QLineF *
             // The interior is below iff the next point ccw order is below the edge
             // THEORY: qi_next needs to be different from p1 or p2
 
-            if (candidate_edge.dx() == 0) {
+            if (candidate_edge.dx() == 0)
+            {
                 continue;
             }
 
-            if (is_upper && (candidate_edge.dy() / candidate_edge.dx())  < 0) {
+            if (is_upper && (candidate_edge.dy() / candidate_edge.dx()) < 0)
+            {
                 continue;
             }
 
-            if (!is_upper && (candidate_edge.dy() / candidate_edge.dx())  > 0) {
+            if (!is_upper && (candidate_edge.dy() / candidate_edge.dx()) > 0)
+            {
                 continue;
             }
             // auto qi_next_index = (qi_index + 1) % polygon.size();
@@ -123,23 +134,19 @@ void ConstantWorkSpace::find_biding_edge(int qi_index, bool * is_found, QLineF *
             // }
             // if (!is_upper && orientation > 0) {
             //     continue;
-            // }   
+            // }
         }
 
         // Now we know this point should be considered
 
-
-        if (!(*is_found) || ((is_upper && intersection.y() < best_intersection_y) || (!is_upper && intersection.y() > best_intersection_y))) {
+        if (!(*is_found) || ((is_upper && intersection.y() < best_intersection_y) || (!is_upper && intersection.y() > best_intersection_y)))
+        {
             *is_found = true;
             *edge = candidate_edge;
             best_intersection_y = intersection.y();
             *edge_start_index = i;
         }
-
-
-
     }
-
 }
 
 void ConstantWorkSpace::pokreniAlgoritam()
@@ -195,14 +202,11 @@ void ConstantWorkSpace::pokreniAlgoritam()
         QPointF eA_intersect_point;
         QPointF eB_intersect_point;
 
-
         int eA_start_index;
         int eB_start_index;
 
         find_biding_edge(qi_index, &ea_initialized, &eA, true, &eA_start_index);
         find_biding_edge(qi_index, &eb_initialized, &eB, false, &eB_start_index);
-
-
 
         // for (auto i = 0; i < polygon.size(); i++)
         // {
@@ -275,17 +279,21 @@ void ConstantWorkSpace::pokreniAlgoritam()
             QPointF replacement;
             int replacement_index = initial_r_index;
 
-            for (int k = 0; k < polygon.size(); k++) {
+            for (int k = 0; k < polygon.size(); k++)
+            {
                 auto inside_point = polygon[k];
-                if (k == qi_index) {
-                    continue;
-                }                
-
-                if (!initial_trapezoid.containsPoint(inside_point, Qt::OddEvenFill)) {
+                if (k == qi_index)
+                {
                     continue;
                 }
 
-                if (!replacement_initialized || inside_point.x() < replacement.x()) {
+                if (!initial_trapezoid.containsPoint(inside_point, Qt::OddEvenFill))
+                {
+                    continue;
+                }
+
+                if (!replacement_initialized || inside_point.x() < replacement.x())
+                {
                     replacement_initialized = true;
                     replacement_index = k;
                     replacement = QPointF(inside_point);
@@ -297,7 +305,6 @@ void ConstantWorkSpace::pokreniAlgoritam()
             std::cerr << "for qi_index=" << qi_index << " we have trapezoid with ea= " << eA_start_index << " eb = " << eB_start_index << " and r = " << replacement_index << std::endl;
 
             trapezoids.push_back(final_trapezoid);
-
         }
 
         eA_display = eA;
@@ -376,11 +383,29 @@ void ConstantWorkSpace::crtajAlgoritam(QPainter *painter) const
     // Draw eA
 
     // std::cerr << "drawing ea from " << ptos(eA.p1()) << " to " << ptos(eA.p2()) << std::endl;
-    pen.setColor(Qt::yellow);
-    pen.setWidth(5);
-    pen.setStyle(Qt::SolidLine);
-    painter->setPen(pen);
-    painter->drawLine(eA_display);
+    if (ea_initialized_display)
+    {
+        pen.setColor(Qt::yellow);
+        pen.setWidth(5);
+        pen.setStyle(Qt::SolidLine);
+        painter->setPen(pen);
+        painter->drawLine(eA_display);
+    }
+
+    // Draw trapezoids
+    for (auto i = 0; i < trapezoids.size(); i++)
+    {
+        auto t = trapezoids[i];
+
+        auto color = colors[i & colors.size()];
+        QBrush b(QColor(color.red(), color.green(), color.blue(), 122));
+        pen.setColor(Qt::black);
+        pen.setWidth(5);
+        pen.setStyle(Qt::DashLine);
+        painter->setPen(pen);
+        painter->setBrush(b);
+        painter->drawPolygon(t);
+    }
 
     // Draw eB
     if (eb_initialized_display)
@@ -402,20 +427,6 @@ void ConstantWorkSpace::crtajAlgoritam(QPainter *painter) const
         painter->drawPoint(replacement_display);
     }
 
-    // Draw trapezoids
-    for (auto i = 0; i < trapezoids.size(); i++)
-    {
-        auto t = trapezoids[i];
-
-        auto color = colors[i & 20];
-        QBrush b(QColor(color.red(), color.green(), color.blue(), 122));
-        pen.setColor(Qt::black);
-        pen.setWidth(5);
-        pen.setStyle(Qt::DashLine);
-        painter->setPen(pen);
-        painter->setBrush(b);
-        painter->drawPolygon(t);
-    }
 }
 
 void ConstantWorkSpace::preparePoints()
